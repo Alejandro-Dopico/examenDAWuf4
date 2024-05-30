41274604ca666c8d7e36ad5611407e7b20bce569
Alejandro Dopico <agarcia2@institutmvm.cat>
2024-05-30 17:25:50 +0200

    Prueba

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 include "hola.php";
-echo “Listado de personas:”
+echo "Listado de personas:"
 include "persona.php";
-include “persona3.php”;
+include "persona3.php";
 ?>
 
